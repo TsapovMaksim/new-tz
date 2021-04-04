880c3f40f665a2c63f643ebf96ba21cabc4ae257
Maxim <tsapov552@gmail.com>
2021-04-04 16:54:09 +0700

    Completed

---
+++ src/components/SomeComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SomeComponent({ children }) {
+  return <div>{children}</div>;
+}
+
+export default SomeComponent;
---
+++ src/components/Validate.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+
+function Validate({ children, isValidate, number }) {
+  function validate() {
+    console.log(`validate component ${number}`);
+  }
+
+  function isComponentEqualValidate(component) {
+    if (React.isValidElement(component) && component.type.name === 'Validate') {
+      return true;
+    }
+    return false;
+  }
+
+  function renderChildren(children, parentValidate = false) {
+    if (!children) {
+      return children;
+    }
+
+    return React.Children.map(children, child => {
+      const isNeedValidate = parentValidate || child.props?.isValidate || false;
+      if (isComponentEqualValidate(child)) {
+        return React.cloneElement(child, {
+          isValidate: isNeedValidate,
+          children: renderChildren(child.props?.children, isNeedValidate),
+        });
+      } else if (React.isValidElement(child)) {
+        return renderChildren(child.props?.children, isNeedValidate);
+      } else {
+        return child;
+      }
+    });
+  }
+
+  useEffect(() => {
+    if (isValidate) {
+      validate();
+    }
+  }, [isValidate]);
+
+  return <div>{renderChildren(children, isValidate)}</div>;
+}
+
+export default Validate;
